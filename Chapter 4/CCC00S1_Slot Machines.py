ae6b9fcb0427c8fbcbe09fe985c031c7acf7ab21
ServontiusT <servontius.turner@me.com>
2021-09-12 20:20:04 -0500

    Fixed input variables and added counter for machine Last Played

--- Chapter 4/CCC00S1_Slot Machines.py
+++ Chapter 4/CCC00S1_Slot Machines.py
@@ -1,41 +1,47 @@
 #CCC '00 S1 - Slot Machines
 # Take user input. Four inputs total. Quarters, Machine 1, Machine 2, Machine 3.
-quarters = input(int())
-machine1PlayedLast = input(int())
-machine2PlayedLast = input(int())
-machine3PlayedLast = input(int())
+quarters = int(input())
+machine1PlayedLast = int(input())
+machine2PlayedLast = int(input())
+machine3PlayedLast = int(input())
 
 currentMachine = 1
 playCounter = 0
 
 # We will need to write a while loop to loop based on the number of quarters Martha has.
-while quarters > 0:
+while quarters >= 1:
     if currentMachine == 1:
         if machine1PlayedLast == 35:
             quarters = quarters + 30
-            currentMachine = 2
+            machine1PlayedLast = 0
             playCounter = playCounter + 1
+            currentMachine = 2
         else:
             quarters = quarters - 1
-            currentMachine = 2
             playCounter = playCounter + 1
-    if currentMachine == 2:
+            machine1PlayedLast = machine1PlayedLast + 1
+            currentMachine = 2
+    elif currentMachine == 2:
         if machine2PlayedLast == 100:
             quarters = quarters + 60
-            currentMachine = 3
             playCounter = playCounter + 1
+            machine2PlayedLast = 0
+            currentMachine = 3
         else:
             quarters = quarters - 1
-            currentMachine = 3
             playCounter = playCounter + 1
-    if currentMachine == 3:
+            machine2PlayedLast = machine2PlayedLast + 1
+            currentMachine = 3
+    elif currentMachine == 3:
         if machine3PlayedLast == 10:
             quarters = quarters + 9
-            currentMachine = 1
             playCounter = playCounter + 1
+            machine3PlayedLast = 0
+            currentMachine = 1
         else:
             quarters = quarters - 1
-            currentMachine = 1
             playCounter = playCounter + 1
+            machine3PlayedLast = machine3PlayedLast + 1
+            currentMachine = 1
 
-print("Martha plays " + playCounter + " times before going broke.")
+print("Martha plays " + str(playCounter) + " times before going broke.")
